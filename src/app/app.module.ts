b7c1844922ec1793ffcae13326a332619ca18073
Gennadii Kruglov <kruglov.gennadii@gmail.com>
2020-09-17 17:23:06 +0300

    Add material.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MaterialModule } from './modules/material/material.module';
+
 
 @NgModule({
   declarations: [
@@ -20,9 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
